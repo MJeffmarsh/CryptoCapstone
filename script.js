1000173b9ac5e44ba3752a39af27e7ab1a489a78
Jeffrey Marshall <46502780+MJeffmarsh@users.noreply.github.com>
2019-09-16 11:59:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 'use strict'
 
-$(selectCoins);
 
 function displayResults(responseJson) {
     console.log(responseJson);
-
-    $("#results-list").empty();
     
-      $('#results-list').append(
-        `<li><h2>${responseJson.name}</h2></li>
+      $('#results').prepend(
+      ` <ul>
+        <li><h2>${responseJson.name}<img src="${responseJson.image.thumb}"></h2></li>
+        <li>Rank: ${responseJson.coingecko_rank}</li>
         <li>Current Price: $${responseJson.market_data.current_price.usd}</li>
-        <li>Market Cap: $${responseJson.market_data.market_cap.usd}</li>`
+        <li>Market Cap: $${responseJson.market_data.market_cap.usd}</li>
+        
+        <button class="remove">remove</button>
+        </ul>
+        `
       );
       $('#results').removeClass('hidden');
   }
   
+  function removeCoin () {
+    $("#results").on('click', '.remove', function(event) {
+      event.preventDefault();
+      this.closest('ul').remove();
+    });
+  }
 
 
 
@@ -31,15 +40,19 @@ function searchCoins(id) {
     .then(responseJson => displayResults(responseJson))
     .catch(err => {
       console.log(err);
-      alert("Something went wrong :(");
+      alert("Coin not found");
     });
 }
+
+
 
 function formSubmit() {
     $('form').submit(event => { event.preventDefault();
     let cryptoSearch = $("#crypto-search").val();
-    searchCoins(cryptoSearch)
+    searchCoins(cryptoSearch);
+    $('#crypto-search').val('')
     });
   }
 
   $(formSubmit);
+  $(removeCoin);
